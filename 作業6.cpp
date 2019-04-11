9efe808d2b25ec9fe1a30e3cc4014fd0c76b1373
richardyjher <48574881+richardyjher@users.noreply.github.com>
2019-04-11 18:25:04 +0800

    Add files via upload

---
+++ 作業6.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	int year;
+	printf("請輸入西元年分:",year);
+	scanf("%d",&year); 
+    if((year%4)==0 && (year%100)!=0 || (year%400) ==0&&(year%3200)!=0)
+    {
+      printf ("%d是閏年\n",year);
+    }
+      else
+    {
+      printf ("%d不是閏年\n",year);
+    } 
+	  system("PAUSE");
+      return 0;
+}
